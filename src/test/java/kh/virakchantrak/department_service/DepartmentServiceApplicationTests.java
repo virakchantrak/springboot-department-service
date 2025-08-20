3bd847e87e025e219dfaabb8b1249c4f041fd8c8
virakchantrak.son <virakchantrak.son@wingbank.com.kh>
2025-08-20 09:51:48 +0700

    First Commit

---
+++ src/test/java/kh/virakchantrak/department_service/DepartmentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package kh.virakchantrak.department_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepartmentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
