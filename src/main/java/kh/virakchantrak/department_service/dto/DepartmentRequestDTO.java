3ac2a302eb04ba1d2c887bbbc83e7d76a3f4819f
virakchantrak.son <virakchantrak.son@wingbank.com.kh>
2025-08-21 10:35:36 +0700

    Add Department RequestDTO

---
+++ src/main/java/kh/virakchantrak/department_service/dto/DepartmentRequestDTO.java
@@ -0,0 +1,12 @@
+package kh.virakchantrak.department_service.dto;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+@Data
+public class DepartmentRequestDTO {
+
+    @NotNull
+    private String name;
+    private String description;
+}
