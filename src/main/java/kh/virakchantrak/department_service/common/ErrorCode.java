eadabfcb198ddab485d1d149d6bec55e45709048
virakchantrak.son <virakchantrak.son@wingbank.com.kh>
2025-08-21 10:36:41 +0700

    Add new file

---
+++ src/main/java/kh/virakchantrak/department_service/common/ErrorCode.java
@@ -0,0 +1,31 @@
+package kh.virakchantrak.department_service.common;
+
+import kh.virakchantrak.library.exception.core.SystemCoreAware;
+import org.springframework.http.HttpStatus;
+
+public enum ErrorCode implements SystemCoreAware {
+    NAME_ALREADY_EXIST("Name already exist", HttpStatus.CONFLICT);
+
+    private final String message;
+    private final HttpStatus status;
+
+    ErrorCode(String message, HttpStatus status) {
+        this.message = message;
+        this.status = status;
+    }
+
+    @Override
+    public String getCode() {
+        return this.name();
+    }
+
+    @Override
+    public String getMessage() {
+        return this.message;
+    }
+
+    @Override
+    public HttpStatus getStatus() {
+        return status;
+    }
+}
---
+++ src/main/java/kh/virakchantrak/department_service/util/DepartmentInit.java
@@ -0,0 +1,35 @@
+package kh.virakchantrak.department_service.util;
+
+import jakarta.annotation.PostConstruct;
+import kh.virakchantrak.department_service.entity.DepartmentEntity;
+import kh.virakchantrak.department_service.repository.DepartmentRepo;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class DepartmentInit {
+    private final DepartmentRepo departmentRepo;
+
+    @PostConstruct
+    public void init() {
+        this.create();
+    }
+
+    public void create() {
+        DepartmentEntity department = new DepartmentEntity();
+        department.setName("IT");
+        department.setDescription("Information Technology Department");
+        departmentRepo.save(department);
+
+        department = new DepartmentEntity();
+        department.setName("HR");
+        department.setDescription("Human Resources Department");
+        departmentRepo.save(department);
+
+        department = new DepartmentEntity();
+        department.setName("Finance");
+        department.setDescription("Finance Department");
+        departmentRepo.save(department);
+    }
+}
