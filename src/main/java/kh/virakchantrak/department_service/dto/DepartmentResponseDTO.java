980a6eb226b8e5fb03e41029c7c41aa8f58b5f92
virakchantrak.son <virakchantrak.son@wingbank.com.kh>
2025-08-21 10:35:47 +0700

    Add Department ResponseDTO

---
+++ src/main/java/kh/virakchantrak/department_service/dto/DepartmentResponseDTO.java
@@ -0,0 +1,10 @@
+package kh.virakchantrak.department_service.dto;
+
+import lombok.Data;
+
+@Data
+public class DepartmentResponseDTO {
+    private Long id;
+    private String name;
+    private String description;
+}
