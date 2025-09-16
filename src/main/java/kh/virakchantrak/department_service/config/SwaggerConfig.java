f97e8ba20ac046f37047eacbff0136db298ff0e5
virakchantrak.son <virakchantrak.son@wingbank.com.kh>
2025-09-16 10:10:55 +0700

    Feat: implement swagger ui open api

---
+++ src/main/java/kh/virakchantrak/department_service/config/SwaggerConfig.java
@@ -0,0 +1,25 @@
+package kh.virakchantrak.department_service.config;
+
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI departmentServiceOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Department Service API")
+                        .description("API documentation for Department Service")
+                        .version("v1.0.0")
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org")))
+                .externalDocs(new ExternalDocumentation()
+                        .description("More Documentation")
+                        .url("https://your-docs-site.com"));
+    }
+}
